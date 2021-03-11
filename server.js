28464c202fd4e08cc488c2e70f81aa2cc7700cfe
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-03-11 13:07:28 -0700

    added .env

--- server.js
+++ server.js
@@ -2,24 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST;
 
-
-
+//Where the HTML + CSS + Scripts + assets go
 app.use(express.static(path.join(__dirname, 'public')));
 
+//dotenv
+require('dotenv').config();
+
 //Listening to port
-app.listen(PORT, function(){
+app.listen(PORT, HOST, function(){
   console.log(`Listening on port ${PORT}`);
 });
 
-require('dotenv').config();
-const db = require('db');
-db.connect({
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.env.DB_PASS
-});
-
+//404 page
 app.use(function(req, res){
   res.status(404);
   res.sendFile(path.join(__dirname,'public/404.html'));
